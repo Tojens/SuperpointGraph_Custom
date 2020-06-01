70f2f98be60167aa1339774645b747fd801e0240
Tojens <44861928+Tojens@users.noreply.github.com>
2020-06-01 12:39:37 +0200

    Initial commit

---
+++ partition/cut-pursuit/build/src/CMakeFiles/cp.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/edisk/superpoint_graph/partition/cut-pursuit/src/cutpursuit.cpp" "/mnt/edisk/superpoint_graph/partition/cut-pursuit/build/src/CMakeFiles/cp.dir/cutpursuit.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "OPENMP"
+  "cp_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/../include"
+  "/mnt/edisk/anaconda3/include"
+  "/mnt/edisk/anaconda3/include/python3.7m"
+  "/mnt/edisk/anaconda3/lib/python3.7/site-packages/numpy/core/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ partition/ply_c/CMakeFiles/ply_c.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/edisk/superpoint_graph/partition/ply_c/ply_c.cpp" "/mnt/edisk/superpoint_graph/partition/ply_c/CMakeFiles/ply_c.dir/ply_c.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ply_c_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/mnt/edisk/anaconda3/include"
+  "/mnt/edisk/anaconda3/include/eigen3"
+  "/mnt/edisk/anaconda3/include/python3.7m"
+  "/mnt/edisk/anaconda3/lib/python3.7/site-packages/numpy/core/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
